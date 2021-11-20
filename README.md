8fa4f200064dc9db6f502277df4fe37675675981
thevickypedia <vignesh.sivanandarao@gmail.com>
2021-11-19 21:56:53 -0600

    Adds CORS policy and update README.md

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # API File Handler
 Upload and download files using FastAPI
+
+PRO-Tip:
+- [localtunnel](https://theboroer.github.io/localtunnel-www/)
+- [ngrok](https://ngrok.com/docs)
--- main.py
+++ main.py
@@ -22,6 +22,7 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    allow_origin_regex='https://.*\.loca\.lt/*',  # noqa: W605
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
